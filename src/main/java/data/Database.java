0b36bff2c9cc33626150899c0d5cc9d5e733439d
Luca Porto Duarte <lduarte@uwo.ca>
2024-11-30 17:45:57 -0500

    Time limit functionality added to backend

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class Database {
     private List<Player> players;
     private String parentalPassword;
+    private long timeLimit;
 
     public Database() {
         players = new ArrayList<>();
         parentalPassword = "";
+        timeLimit = 0;
     }
 
     public List<Player> getPlayers() {
@@ -31,5 +33,13 @@ public class Database {
 
     public void setParentalPassword(String password) {
         this.parentalPassword = password;
+    }
+
+    public long getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(long timeLimit) {
+        this.timeLimit = timeLimit;
     }
 }
