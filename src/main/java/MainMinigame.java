35a7b296dc127851f7c99afb6f256b5d13ed1c0c
Ali Farhangi <afarha@uwo.ca>
2024-11-30 23:04:41 -0500

    Added something I forgot

--- src/main/java/MainMinigame.java
+++ src/main/java/MainMinigame.java
@@ -43,27 +43,3 @@ public class MainMinigame {
     }
 }
 
-//import javax.swing.*;
-//
-//public class MainMinigame {
-//    public static void main(String[] args) {
-//        new LevelSelect();
-//    }
-//    public MainMinigame(String levelName) {
-//        JFrame window = new JFrame();
-//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        WindowManager.setWindow(window); // Store the JFrame globally
-//
-//        GamePanel gamePanel = new GamePanel(levelName);
-//        window.add(gamePanel);
-//
-//        window.pack();
-//        window.setLocationRelativeTo(null);
-//        window.setVisible(true);
-//
-//        gamePanel.setUpGame();
-//        gamePanel.startGameThread();
-//    }
-//}
-//
--- src/main/java/OBJ_EvolutionFruit.java
+++ src/main/java/OBJ_EvolutionFruit.java
@@ -28,20 +28,3 @@ public class OBJ_EvolutionFruit extends SuperObject {
 }
 
 
-//import javax.imageio.ImageIO;
-//import java.io.IOException;
-//
-//public class OBJ_EvolutionFruit extends SuperObject {
-//
-//    public OBJ_EvolutionFruit(){
-//
-//        name="Evolution Fruit";
-//
-//        try{
-//            image = ImageIO.read(getClass().getResourceAsStream("evolutionFruit.png"));
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        collision = true;
-//    }
-//}
